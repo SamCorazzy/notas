7eca04187e3881d02f6f458c90a46e246481e933
Samuel Perez Reyes <corazzy@hotmail.com>
2022-04-27 09:54:20 -0500

    Boton de guardar

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,42 @@ import { useState } from "react";
 
 function App() {
 
-//TODO: Presentar el concepto "State"
+  //TODO: Presentar el concepto "State"
 
-//hooks
-const [inputState, setInputState] = useState({
-  titulo: "",
-  fecha: "",
-  nota: "",
-});
-// const [fechaState, setFechaState] = useState("Fecha");
-// const [notaState, setNotaState] = useState("Nota");
+  //hooks
+  const [inputState, setInputState] = useState({
+    titulo: "",
+    fecha: "",
+    nota: "",
+  });
+  // const [fechaState, setFechaState] = useState("Fecha");
+  // const [notaState, setNotaState] = useState("Nota");
 
 
-  const handleInputChange = (event) => { 
+  const handleInputChange = (event) => {
     //console.log(event.target)
     setInputState({
       ...inputState,
       [event.target.name]: event.target.value,
     });
     console.log(inputState);
-   }
+  };
+
+  const handleResetChange = () => {
+    setInputState({
+      ...inputState,
+      titulo: "",
+      fecha: "",
+      nota: "",
+    });
+  };
+
+  const handleClicGuardar = () => {
+    let arregloNotas = JSON.parse(localStorage.getItem("notas")) || [];
+    arregloNotas.push(inputState);
+    localStorage.setItem("notas", JSON.stringify(arregloNotas));
+    handleResetChange();
+  };
 
   //  const handleChangeFecha = (event) => { 
   //   setFechaState(event.target.value);
@@ -34,34 +50,58 @@ const [inputState, setInputState] = useState({
   //  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Notas</h1>
-        <label htmlFor="titulo" className="pe-4">Input de titulo:</label>
-        <input 
-          id= "titulo" 
-          name="titulo" 
-          type="text" 
-          onChange={handleInputChange}
-          value={inputState.titulo}
-        /> <br /><br />
-        <label htmlFor="fecha" className="pe-4">Input de fecha:</label>
-        <input 
-          id= "fecha" 
-          name="fecha" 
-          type="text" 
-          onChange={handleInputChange}
-          value={inputState.fecha}
-        /> <br /><br />
-        <label htmlFor="nota" className="pe-4">Input de nota:</label>
-        <input 
-          id= "nota" 
-          name="nota" 
-          type="text" 
-          onChange={handleInputChange}
-          value={inputState.nota}
-        />
-      </header>
+    <div className="App container">
+      <div className="row text-center">
+        <div className="col p-4">
+          <h3>Lista</h3>
+        </div>
+        <div className="col text-center mx-auto p-4">
+          <h3>Notas</h3>
+          <label htmlFor="titulo" className="pe-4" style={{ width: "100%" }}>Input de titulo:</label>
+          <input
+            id="titulo"
+            name="titulo"
+            type="text"
+            style={{ width: "100%" }}
+            onChange={handleInputChange}
+            value={inputState.titulo}
+          /> <br /><br />
+          <label htmlFor="fecha" className="pe-4" style={{ width: "100%" }}>Input de fecha:</label>
+          <input
+            id="fecha"
+            name="fecha"
+            type="date"
+            style={{ width: "100%" }}
+            onChange={handleInputChange}
+            value={inputState.fecha}
+          /> <br /><br />
+          <label htmlFor="nota" className="pe-4" style={{ width: "100%" }}>Input de nota:</label>
+          <input
+            id="nota"
+            name="nota"
+            type="text"
+            style={{ width: "100%" }}
+            onChange={handleInputChange}
+            value={inputState.nota}
+          />
+          <br />
+          <hr />
+          <div className="ms-2 me-2 mt-2 row">
+            <div className="col">
+              <span className="row mx-1">
+                <button class="mx-auto" onClick={handleResetChange} style={{ marginLeft: "15px" }}>Borrar</button>
+              </span>
+
+            </div>
+            <div className="col">
+              <span className="row mx-1">
+                <button class="mx-auto" onClick={handleClicGuardar} style={{ marginLeft: "15px" }}>Guardar</button>
+              </span>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
